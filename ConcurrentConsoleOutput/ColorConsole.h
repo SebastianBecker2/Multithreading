7b1a3760b0921d878526f66d11037a9fd0668182
Sebastian Becker <mail@sbecker.de.com>
2020-12-12 13:17:33 +0100

    Apply code reformat

--- ConcurrentConsoleOutput/ColorConsole.h
+++ ConcurrentConsoleOutput/ColorConsole.h
@@ -1,54 +1,56 @@
 #pragma once
 
+#include <Windows.h>
 #include <iostream>
 #include <unordered_map>
 
-#include <Windows.h>
-
-enum class ConsoleColor {
-  Red = FOREGROUND_RED,
-  Green = FOREGROUND_GREEN,
-  Blue = FOREGROUND_BLUE,
-  Yellow = Red | Green,
-  Cyan = Blue | Green,
-  Magenta = Red | Blue,
-  LightRed = FOREGROUND_RED | FOREGROUND_INTENSITY,
-  LightGreen = FOREGROUND_GREEN | FOREGROUND_INTENSITY,
-  LightBlue = FOREGROUND_BLUE | FOREGROUND_INTENSITY,
-  LightYellow = Yellow | FOREGROUND_INTENSITY,
-  LightCyan = Cyan | FOREGROUND_INTENSITY,
-  LightMagenta = Magenta | FOREGROUND_INTENSITY,
-  Black = 0,
-  White = Red | Green | Blue,
+enum class ConsoleColor
+{
+    Red = FOREGROUND_RED,
+    Green = FOREGROUND_GREEN,
+    Blue = FOREGROUND_BLUE,
+    Yellow = Red | Green,
+    Cyan = Blue | Green,
+    Magenta = Red | Blue,
+    LightRed = FOREGROUND_RED | FOREGROUND_INTENSITY,
+    LightGreen = FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    LightBlue = FOREGROUND_BLUE | FOREGROUND_INTENSITY,
+    LightYellow = Yellow | FOREGROUND_INTENSITY,
+    LightCyan = Cyan | FOREGROUND_INTENSITY,
+    LightMagenta = Magenta | FOREGROUND_INTENSITY,
+    Black = 0,
+    White = Red | Green | Blue,
 };
 
 std::ostream& operator<<(std::ostream& os, const ConsoleColor& cc)
 {
-  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-  SetConsoleTextAttribute(hConsole, static_cast<int>(cc));
-  return os;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, static_cast<int>(cc));
+    return os;
 }
 
-namespace std {
-namespace {
-  std::unordered_map<ConsoleColor, std::string> ConsoleColorNames{
-    { ConsoleColor::Red, "Red" },
-    { ConsoleColor::Green, "Green" },
-    { ConsoleColor::Blue, "Blue" },
-    { ConsoleColor::Yellow, "Yellow" },
-    { ConsoleColor::Cyan, "Cyan" },
-    { ConsoleColor::Magenta, "Magenta" },
-    { ConsoleColor::LightRed, "LightRed" },
-    { ConsoleColor::LightGreen, "LightGreen" },
-    { ConsoleColor::LightBlue, "LightBlue" },
-    { ConsoleColor::LightYellow, "LightYellow" },
-    { ConsoleColor::LightCyan, "LightCyan" },
-    { ConsoleColor::LightMagenta, "LightMagenta" },
-    { ConsoleColor::White, "White" },
-  };
+namespace std
+{
+namespace
+{
+    std::unordered_map<ConsoleColor, std::string> ConsoleColorNames{
+        {ConsoleColor::Red, "Red"},
+        {ConsoleColor::Green, "Green"},
+        {ConsoleColor::Blue, "Blue"},
+        {ConsoleColor::Yellow, "Yellow"},
+        {ConsoleColor::Cyan, "Cyan"},
+        {ConsoleColor::Magenta, "Magenta"},
+        {ConsoleColor::LightRed, "LightRed"},
+        {ConsoleColor::LightGreen, "LightGreen"},
+        {ConsoleColor::LightBlue, "LightBlue"},
+        {ConsoleColor::LightYellow, "LightYellow"},
+        {ConsoleColor::LightCyan, "LightCyan"},
+        {ConsoleColor::LightMagenta, "LightMagenta"},
+        {ConsoleColor::White, "White"},
+    };
 }
 const std::string& to_string(const ConsoleColor& cc)
 {
-  return ConsoleColorNames[cc];
+    return ConsoleColorNames[cc];
 }
-}// namespace std
+} // namespace std
--- ConcurrentConsoleOutput/Constants.h
+++ ConcurrentConsoleOutput/Constants.h
@@ -1,24 +1,18 @@
 #pragma once
 
+#include "ColorConsole.h"
+
 #include <chrono>
 #include <vector>
 
-#include "ColorConsole.h"
-
-static constexpr int IterationsPerThread{ 5 };
-static constexpr std::chrono::milliseconds SleepPerIteration{ 10 };
+static constexpr int IterationsPerThread{5};
+static constexpr std::chrono::milliseconds SleepPerIteration{10};
 static std::vector<ConsoleColor> ConsoleColors{
-  ConsoleColor::Red,
-  ConsoleColor::Green,
-  ConsoleColor::Blue,
-  ConsoleColor::Yellow,
-  ConsoleColor::Cyan,
-  ConsoleColor::Magenta,
-  ConsoleColor::LightRed,
-  ConsoleColor::LightGreen,
-  ConsoleColor::LightBlue,
-  ConsoleColor::LightYellow,
-  ConsoleColor::LightCyan,
-  ConsoleColor::LightMagenta,
-  ConsoleColor::White,
+    ConsoleColor::Red,       ConsoleColor::Green,
+    ConsoleColor::Blue,      ConsoleColor::Yellow,
+    ConsoleColor::Cyan,      ConsoleColor::Magenta,
+    ConsoleColor::LightRed,  ConsoleColor::LightGreen,
+    ConsoleColor::LightBlue, ConsoleColor::LightYellow,
+    ConsoleColor::LightCyan, ConsoleColor::LightMagenta,
+    ConsoleColor::White,
 };
