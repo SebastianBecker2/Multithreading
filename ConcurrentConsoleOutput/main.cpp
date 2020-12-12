18aec5810ce19ee453255ec06eec27b5a59cecaf
Sebastian Becker <mail@sbecker.de.com>
2020-12-12 13:24:35 +0100

    Move constants back into main.cpp

--- ConcurrentConsoleOutput/main.cpp
+++ ConcurrentConsoleOutput/main.cpp
@@ -1,5 +1,4 @@
 #include "ColorConsole.h"
-#include "Constants.h"
 
 #include <chrono>
 #include <iomanip>
@@ -10,6 +9,18 @@
 
 #undef USE_MUTEX
 std::mutex mutex;
+
+static constexpr int IterationsPerThread{5};
+static constexpr std::chrono::milliseconds SleepPerIteration{10};
+static std::vector<ConsoleColor> ConsoleColors{
+    ConsoleColor::Red,       ConsoleColor::Green,
+    ConsoleColor::Blue,      ConsoleColor::Yellow,
+    ConsoleColor::Cyan,      ConsoleColor::Magenta,
+    ConsoleColor::LightRed,  ConsoleColor::LightGreen,
+    ConsoleColor::LightBlue, ConsoleColor::LightYellow,
+    ConsoleColor::LightCyan, ConsoleColor::LightMagenta,
+    ConsoleColor::White,
+};
 
 void OutputColoredText(ConsoleColor color)
 {
